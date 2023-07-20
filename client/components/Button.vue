cbf9984a994c150c2be0c42559acd39fa50e1b38
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2023-07-20 12:22:33 +0200

    Creation of component Button.vue Adjustement in the first 2 section of index.vue

---
+++ client/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <nuxt-link
+    :to="destination"
+    :class="{
+        'bg-pinky-800 hover:bg-pinky-500': color === 'burgundy',
+        'bg-pinky-1000 hover:bg-pinky-900': color === 'black',
+    }"
+    class="text-white font-serif text-lg px-4 py-3 rounded-lg transition-colors duration-500"
+    >
+        <slot></slot>
+    </nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        color: {
+            type: String,
+            default: 'burgundy',
+        },
+        destination: {
+            type: String,
+            default: '/',
+        },
+    },
+};
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-5xl h-min mx-auto flex flex-col">
+    <section class="max-w-5xl h-min mx-auto flex flex-col">
         <div class="text-center">
             <h1 class="my-auto text-wide whitespace-nowrap lg:text-xxl xl:text-xxl">
                 MELODY SYNC
@@ -12,44 +12,40 @@
             celebrate the joys of mastering musical instruments. Dive in and join the melody!
         </p>
 
-        <div class="m-10">
-            <button
-                class="text-white font-serif text-lg bg-pinky-800 hover:bg-pinky-500 px-4 py-2 rounded-lg transition-colors duration-500">
-                Sign Up
-            </button>
-            <button
-                class="text-white font-serif text-lg bg-pinky-1000 hover:bg-pinky-900 px-4 py-2 rounded-lg transition-colors duration-500 ml-8">
-                Login
-            </button>
+        <div class="ml-10 my-20">
+          <Button color="burgundy" destination="/sign-up">Sign Up</Button>
+          <Button color="black" destination="/login" class="ml-8">Login</Button>
         </div>
-    </div>
+      </section>
 
 
-    <div class="bg-pinky-900 h-min text-pinky-50 text-lg py-24">
-        <div class="flex w-4/5 mx-auto">
-            <div class="h-min max-w-md">
-                <img src="../static/images/Orchestra.png" class="mx-auto rounded-3xl mix-blend-luminosity" alt="orchestra">
-            </div>
+    <section class="bg-pinky-900 h-min text-pinky-50 text-lg py-24 mx-auto">
+      <div class="grid grid-cols-2 grid-rows-2 w-4/5 mx-auto gap-16">
+        <img src="../static/images/Orchestra.png" class="index-image justify-self-end" alt="orchestra">
 
-            <div class="flex flex-col justify-center ml-10 max-w-md">
-                <h2 class="text-4xl">Share Advice, Learn Faster, and Make Friends</h2>
-                <p class="font-serif mt-6">Melody Sync brings together ambitious learners to motivate, inspire, and support
-                    each
-                    other on their musical journeys. Why learn alone when you can grow together?</p>
-            </div>
+        <article class="flex flex-col justify-center">
+          <h2 class="text-4xl">Share Advice, Learn Faster, and Make Friends</h2>
+          <p class="font-serif mt-6">Melody Sync brings together ambitious learners to motivate, inspire, and support each
+            other on their musical journeys. Why learn alone when you can grow together?</p>
+        </article>
+
+        <div class="flex flex-col justify-center text-right">
+          <h2 class="text-4xl">Create Profiles to Display Your Instrument Mastery</h2>
+          <p class="font-serif mt-6">Show off your favorite instrument and the skills you’re acquiring. Be proud of your
+            progress and encourage others to reach new heights!</p>
         </div>
-
-        <div class="flex w-4/5 mx-auto mt-20">
-            <div class="flex flex-col justify-center ml-10 max-w-md">
-                <h2 class="text-4xl">Create Profiles to Display Your Instrument Mastery</h2>
-                <p class="font-serif mt-6">Show off your favorite instrument and the skills you’re acquiring. Be proud of
-                    your
-                    progress and encourage others to reach new heights!</p>
-            </div>
-            <div class="h-min max-w-md">
-                <img src="../static/images/Guitar.png" class="mx-auto rounded-3xl mix-blend-luminosity" alt="guitar">
-            </div>
-        </div>
-    </div>
+        
+        <img src="../static/images/Guitar.png" class="index-image justify-self-start" alt="guitar">
+      </div>
+    </section>
 </template>
+
+<style>
+.index-image {
+  border-radius: 1.5rem;
+  mix-blend-mode: luminosity;
+  min-height: auto;
+  max-width: 28rem;
+}
+</style>
   
