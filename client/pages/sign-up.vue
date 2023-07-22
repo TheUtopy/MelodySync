59b9b3560e47987fb8d5e6917f91a9d192c4ce1f
TheUtopy <kevin.chron@gmail.com>
2023-07-22 16:26:33 +0200

    Sign in page linked to the API Error handling incoming

--- client/pages/sign-up.vue
+++ client/pages/sign-up.vue
@@ -6,17 +6,24 @@
         <div class="mx-auto">
           <h2 class="pb-8">Sign Up</h2>
           <form @submit="submitForm" class="text-pinky-1000 font-serif pb-6">
-            <FieldInForm label="Username" inputId="username" inputType="text" :input-value="username" @update:input-value="username = $event"/>
-            <FieldInForm label="Email" inputId="email" inputType="email" :input-value="email" @update:input-value="email = $event"/>
-            <FieldInForm label="Password" inputId="password" inputType="password" :input-value="password" @update:input-value="password = $event"/>
-            <FieldInForm label="Confirm Password" inputId="confirmPassword" inputType="password" :input-value="confirmPassword" @update:input-value="confirmPassword = $event"/>
+            <FieldInForm label="Username" inputId="username" inputType="text" :input-value="username"
+              @update:input-value="username = $event" />
+            <FieldInForm label="Email" inputId="email" inputType="email" :input-value="email"
+              @update:input-value="email = $event" />
+            <FieldInForm label="Password" inputId="password" inputType="password" :input-value="password"
+              @update:input-value="password = $event" />
+            <FieldInForm label="Confirm Password" inputId="confirmPassword" inputType="password"
+              :input-value="confirmPassword" @update:input-value="confirmPassword = $event" />
             <Button color="red" is-submit-button @submit-form="submitForm">Submit</Button>
           </form>
-          <h3>Already have an account? <NuxtLink to="/login" class="underline hover:no-underline hover:text-pink-700 visited:text-pink-800">Login</NuxtLink></h3>
+          <h3>Already have an account? <NuxtLink to="/login"
+              class="underline hover:no-underline hover:text-pink-700 visited:text-pink-800">Login</NuxtLink>
+          </h3>
         </div>
         <div class="pt-[4.4rem] mx-auto flex flex-col items-center">
           <Image source="../static/images/Violin.png" alt="violin" class="max-w-sm lg:max-w-lg" />
-          <p class="max-w-sm lg:max-w-lg text-sans text-3xl text-pink-100 text-center pt-8">Don't miss a beat! Sign in now and start amplifying your musical journey.</p>
+          <p class="max-w-sm lg:max-w-lg text-sans text-3xl text-pink-100 text-center pt-8">Don't miss a beat! Sign in now
+            and start amplifying your musical journey.</p>
         </div>
       </article>
       <Footer class="text-center py-10" />
@@ -37,25 +44,40 @@ export default {
     };
   },
   methods: {
-      submitForm(e) {
-          e.preventDefault();
-          // Vérification des champs requis
-          if (this.username === '') {
-          this.errorMessage['username'] = 'Username is required';
-          };
-          if (this.email === '') {
-          this.errorMessage['email'] = 'Email is required';
-          };
-          if (this.password === '') {
-          this.errorMessage['password'] = 'Password is required';
-          };
+    async submitForm(e) {
+      e.preventDefault();
+      // Vérification des champs requis
+      if (this.username === '') {
+        this.errorMessage['username'] = 'Username is required';
+      };
+      if (this.email === '') {
+        this.errorMessage['email'] = 'Email is required';
+      };
+      if (this.password === '') {
+        this.errorMessage['password'] = 'Password is required';
+      };
 
-          // Logique de soumission du formulaire
-          if (this.username !== '' && this.email !== '' && this.password !== '' && this.confirmPassword !== '') {
-          console.log(this.username, this.email, this.password, this.confirmPassword);
-          // Soumettre le formulaire
-          }
-      },
+      // Logique de soumission du formulaire
+      if (this.username !== '' && this.email !== '' && this.password !== '' && this.confirmPassword !== '') {
+        console.log(this.username, this.email, this.password, this.confirmPassword);
+        // Soumettre le formulaire
+        const formData = {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        };
+
+        try {
+          const response = await fetch('http://localhost:8000/api/user/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
   },
 };
 </script>
--- server/server/settings.py
+++ server/server/settings.py
@@ -133,6 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'MelodySync.User'
 
-# Do not use this in production :
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
