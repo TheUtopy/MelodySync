fbbdacca5e9b450191fb9f1a93f30df05ba51837
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2023-08-09 16:05:14 +0200

    Front : Header component text is now a link

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="text-center">
+        <nuxt-link to="/">
         <h1 class="my-auto text-wide whitespace-nowrap lg:text-xxl xl:text-xxl">
             MELODY SYNC
         </h1>
+        </nuxt-link>
     </header>
 </template>
